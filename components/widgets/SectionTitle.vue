385dd5ce0dd9cda313be0151fe5ec891dd324f5d
Szymon Wójcik <wojcik.szymon1@gmail.com>
2025-02-13 17:57:17 +0100

    feat: section title

---
+++ components/widgets/SectionTitle.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Props {
+  title: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="title">
+    {{ title }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.title {
+  color: $primary-700-color;
+  font-weight: bold;
+  font-size: 2rem;
+  margin: 2rem 0;
+}
+</style>
